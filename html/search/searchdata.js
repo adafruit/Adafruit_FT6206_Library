afb22a2287b55cc48f2eead8248da943bd4c59c3
Travis CI <travis@travis-ci.org>
2018-01-15 04:20:17 +0000

    Deploy code docs to GitHub Pages Travis build: 35
    Commit: 013086b1f5b03122526a94db55de13a46c2ab501

---
+++ html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "abgt",
+  1: "at",
+  2: "abgt",
+  3: "a"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions",
+  3: "Pages"
+};
+
