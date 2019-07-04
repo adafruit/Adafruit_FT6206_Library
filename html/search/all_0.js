10f1046325d2e7909a716d3609e235256893ddba
Travis CI <travis@travis-ci.org>
2019-07-04 09:58:06 +0000

    Deploy code docs to GitHub Pages Travis build: 48
    Commit: 0360ed38c5cd5d9c118ea55d83d33f876dc138a1

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fft6206',['Adafruit_FT6206',['../class_adafruit___f_t6206.html',1,'Adafruit_FT6206'],['../class_adafruit___f_t6206.html#aa35eb0e3808a481c814492a8c875f10d',1,'Adafruit_FT6206::Adafruit_FT6206()']]],
-  ['adafruit_20ft6206_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fft6206_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fft6206_5flibrary_29',['Adafruit FT6206 Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_FT6206_Library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_FT6206_Library)',['../md__home_travis_build_adafruit__adafruit__f_t6206__library__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_5fft6206',['Adafruit_FT6206',['../class_adafruit___f_t6206.html',1,'Adafruit_FT6206'],['../class_adafruit___f_t6206.html#aa35eb0e3808a481c814492a8c875f10d',1,'Adafruit_FT6206::Adafruit_FT6206()']]]
 ];
